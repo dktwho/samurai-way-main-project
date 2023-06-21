829b246d1bdbb4503ae5017ae8f10531adf7f2b2
Alexander Korobov <dktwho@mail.ru>
2023-06-21 15:53:18 +0300

    add basic structure, grid templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Technologies from "./Technologies";
-import { Footer } from "./Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1> Social Network</h1>
-      <Technologies />
-      <Header />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://seeklogo.com/images/S/snapsvg-logo-8E936C9AE9-seeklogo.com.png"
+          alt="logo"
+        />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="">Profile</a>
+        </div>
+        <div>
+          <a href="">Messages</a>
+        </div>
+        <div>
+          <a href="">News</a>
+        </div>
+        <div>
+          <a href="">Music</a>
+        </div>
+        <div>
+          <a href="">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img
+            src="https://www.atlantictours.com/images/stories/com_form2content/p1/f103/nova-scotia-halifax-waterfront-harbour-hopper-purdys-wharf-875x375.jpg"
+            alt=""
+          />
+        </div>
+        <div>Ava + description</div>
+        <div>
+          My Posts
+          <div>New post</div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
     </div>
   );
 };
