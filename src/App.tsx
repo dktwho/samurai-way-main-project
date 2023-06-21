9eaf43f06033fab96eed74c295026bec8238b1b4
Alexander Korobov <dktwho@mail.ru>
2023-06-21 10:32:33 +0300

    create apart component Header and Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Technologies from "./Technologies";
 
 const App = () => {
   return (
@@ -7,29 +9,6 @@ const App = () => {
       <h1> Social Network</h1>
       <Technologies />
       <Header />
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#s">Home</a>
-      <a href="#s">New Feed</a>
-      <a href="#s">Messages</a>
-    </div>
-  );
-};
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>React</li>
-      </ul>
     </div>
   );
 };
