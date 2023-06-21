6bb06313f8424ffe1541bf1c5748d6038d13b4af
Alexander Korobov <dktwho@mail.ru>
2023-06-21 10:04:42 +0300

    practice with create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <h1> Social Network</h1>
+      <Technologies />
+      <Header />
     </div>
   );
-}
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">New Feed</a>
+      <a href="#s">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
