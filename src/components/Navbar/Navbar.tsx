5bf25abc685d7447447caa46655cb6f8ca55e5f2
Alexander Korobov <dktwho@mail.ru>
2023-07-04 12:00:34 +0300

    add react-router-dom v5, add path for dialog, profile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import styled from "./Navbar.module.css";
 
 export const Navbar = () => {
-  return (
-    <nav className={styled.nav}>
-      <div className={`${styled.item} ${styled.active}`}>
-        <a>Profile</a>
-      </div>
-      <div className={styled.item}>
-        <a>Messages</a>
-      </div>
-      <div className={styled.item}>
-        <a>News</a>
-      </div>
-      <div className={styled.item}>
-        <a>Music</a>
-      </div>
-      <div className={styled.item}>
-        <a>Settings</a>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={styled.nav}>
+            <div className={`${styled.item} ${styled.active}`}>
+                <a href='/profile'>Profile</a>
+            </div>
+            <div className={styled.item}>
+                <a href='/dialogs'>Messages</a>
+            </div>
+            <div className={styled.item}>
+                <a>News</a>
+            </div>
+            <div className={styled.item}>
+                <a>Music</a>
+            </div>
+            <div className={styled.item}>
+                <a>Settings</a>
+            </div>
+        </nav>
+    );
 };
