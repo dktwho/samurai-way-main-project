8d17aa43cd067a28941b482cae5eb7ddc7d196d6
Alexander Korobov <dktwho@mail.ru>
2023-07-04 14:15:26 +0300

    Create component DialogItem add props^ name, id for url path and typed

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import styled from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+
+type DialogItemType = {
+    name: string;
+    id: number;
+
+}
+const DialogItem = ({name, id}: DialogItemType) => {
+    return (
+        <div className={`${styled.dialog} ${styled.active}`}>
+            <NavLink to={`/dialogs/${id}`}>{name}</NavLink>
+        </div>
+    )
+}
+
 export const Dialogs = () => {
     return (
         <div className={styled.dialogs}>
             <div className={styled.dialogsItem}>
-                <div className={`${styled.dialog} ${styled.active}`}>
-                    <NavLink to='/dialogs/1'>Bob</NavLink>
-                </div>
-
-                <div className={styled.dialog}>
-                    <NavLink to='/dialogs/2'>Bill</NavLink>
-                </div>
-
-                <div className={styled.dialog}>
-                    <NavLink to='/dialogs/3'>Tedd</NavLink>
-                </div>
-
-                <div className={styled.dialog}>
-                    <NavLink to='/dialogs/3'>Sam</NavLink>
-                </div>
-
+                <DialogItem name={'Bob'} id={1}/>
+                <DialogItem name={'Bill'} id={2}/>
+                <DialogItem name={'Sam'} id={3}/>
+                <DialogItem name={'Tedd'} id={4}/>
+                <DialogItem name={'John'} id={5}/>
             </div>
 
             <div className={styled.messages}>
