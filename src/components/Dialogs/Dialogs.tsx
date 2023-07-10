dff6de36c8b46e80a84a082a2aa289ec298d24b5
Alexander Korobov <dktwho@mail.ru>
2023-07-10 12:12:35 +0300

    create hardcode array with obj data, dialogsData and messagesData

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,8 +6,9 @@ import {NavLink} from "react-router-dom";
 type DialogItemType = {
     name: string;
     id: number;
-
 }
+
+
 const DialogItem: React.FC<DialogItemType> = ({name, id}) => {
     let path = `/dialogs/${id}`
     return (
@@ -20,6 +21,7 @@ const DialogItem: React.FC<DialogItemType> = ({name, id}) => {
 
 export type MessageType = {
     message: string
+    id: number
 }
 const Message: React.FC<MessageType> = ({message}) => {
     return (
@@ -28,20 +30,41 @@ const Message: React.FC<MessageType> = ({message}) => {
 }
 
 export const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Bob'},
+        {id: 2, name: 'Bill'},
+        {id: 3, name: 'Sam'},
+        {id: 4, name: 'Tedd'},
+        {id: 5, name: 'John'},
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Hello'},
+        {id: 3, message: 'How are you?'},
+        {id: 4, message: 'Fine'},
+        {id: 4, message: 'Good'},
+    ]
+
     return (
         <div className={styled.dialogs}>
             <div className={styled.dialogsItem}>
-                <DialogItem name={'Bob'} id={1}/>
-                <DialogItem name={'Bill'} id={2}/>
-                <DialogItem name={'Sam'} id={3}/>
-                <DialogItem name={'Tedd'} id={4}/>
-                <DialogItem name={'John'} id={5}/>
+                {dialogsData.map(elem => {
+                    return (
+                        <DialogItem key={elem.id} name={elem.name} id={elem.id}/>
+                    )
+                })}
+
             </div>
 
             <div className={styled.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'Hello'}/>
-                <Message message={'How are you?'}/>
+                {messagesData.map(elem => {
+                    return (
+                        <Message key={elem.id} message={elem.message} id={elem.id}/>
+                    )
+                })}
+
             </div>
         </div>
     );
