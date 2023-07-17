5f167f6af45fbb416df1bc0701e9e552ecf76fa0
Alexander Korobov <dktwho@mail.ru>
2023-07-17 18:37:00 +0300

    create apart file state with all data, import inside index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,33 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {state} from "./redux/state";
 
-const postsData = [
-    {id: 1, message: 'Post 1', likesCount: '13'},
-    {id: 2, message: 'Post 2', likesCount: '22'},
-    {id: 3, message: 'Post 3', likesCount: '63'},
-    {id: 4, message: 'Post 4', likesCount: '45'},
-    {id: 5, message: 'Post 5', likesCount: '95'},
-]
-
-let dialogsData = [
-    {id: 1, name: 'Bob'},
-    {id: 2, name: 'Bill'},
-    {id: 3, name: 'Sam'},
-    {id: 4, name: 'Tedd'},
-    {id: 5, name: 'John'},
-]
-
-let messagesData = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'Hello'},
-    {id: 3, message: 'How are you?'},
-    {id: 4, message: 'Fine'},
-    {id: 5, message: 'Good'},
-]
-
+const {messages, posts, dialogs} = state
 
 ReactDOM.render(
-    <App posts={postsData} dialogsData={dialogsData} messagesData={messagesData}/>,
+    <App posts={posts} dialogsData={dialogs} messagesData={messages}/>,
     document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+export let state = {
+    posts: [
+        {id: 1, message: 'Post 1', likesCount: '13'},
+        {id: 2, message: 'Post 2', likesCount: '22'},
+        {id: 3, message: 'Post 3', likesCount: '63'},
+        {id: 4, message: 'Post 4', likesCount: '45'},
+        {id: 5, message: 'Post 5', likesCount: '95'},
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Hello'},
+        {id: 3, message: 'How are you?'},
+        {id: 4, message: 'Fine'},
+        {id: 5, message: 'Good'},
+    ],
+    dialogs: [
+        {id: 1, name: 'Bob'},
+        {id: 2, name: 'Bill'},
+        {id: 3, name: 'Sam'},
+        {id: 4, name: 'Tedd'},
+        {id: 5, name: 'John'},
+    ]
+}
+
