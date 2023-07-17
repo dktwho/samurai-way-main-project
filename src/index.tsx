c818e2a83f7e740cd96225c8a09f2ff3bf7ef3c8
Alexander Korobov <dktwho@mail.ru>
2023-07-17 18:25:55 +0300

    move all data to index tsx, type all props data, fixed

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const postsData  = [
+const postsData = [
     {id: 1, message: 'Post 1', likesCount: '13'},
     {id: 2, message: 'Post 2', likesCount: '22'},
     {id: 3, message: 'Post 3', likesCount: '63'},
@@ -19,7 +19,7 @@ let dialogsData = [
     {id: 5, name: 'John'},
 ]
 
-let messagesData  = [
+let messagesData = [
     {id: 1, message: 'Hi'},
     {id: 2, message: 'Hello'},
     {id: 3, message: 'How are you?'},
@@ -28,8 +28,7 @@ let messagesData  = [
 ]
 
 
-
 ReactDOM.render(
-    <App posts={postsData} dialogsData={dialogsData} messagesData={messagesData}  />,
-  document.getElementById('root')
+    <App posts={postsData} dialogsData={dialogsData} messagesData={messagesData}/>,
+    document.getElementById('root')
 );
