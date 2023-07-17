f88e6fa2963674646eef1b41c48e28ec20d5e0b8
Alexander Korobov <dktwho@mail.ru>
2023-07-17 17:43:41 +0300

    move data from app to one level up in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,11 @@ import {Profile} from "./components/Profile/Profile";
 import "./App.css";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import {PostsTypeProps} from "./components/Profile/MyPosts/MyPosts";
 
 
 
-const App = () => {
-
-    const postsData  = [
-        {id: 1, message: 'Post 1', likesCount: '13'},
-        {id: 2, message: 'Post 2', likesCount: '22'},
-        {id: 3, message: 'Post 3', likesCount: '63'},
-        {id: 4, message: 'Post 4', likesCount: '45'},
-        {id: 5, message: 'Post 5', likesCount: '95'},
-    ]
-
+const App = (props: PostsTypeProps) => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
@@ -28,7 +20,7 @@ const App = () => {
                     {/*<Route  path='/profile' component={Profile}/>*/}
 
                     <Route  path='/dialogs' render={() => <Dialogs/>}/>
-                    <Route  path='/profile' render={() => <Profile posts={postsData}/>}/>
+                    <Route  path='/profile' render={() => <Profile posts={props.posts}/>}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const postsData  = [
+    {id: 1, message: 'Post 1', likesCount: '13'},
+    {id: 2, message: 'Post 2', likesCount: '22'},
+    {id: 3, message: 'Post 3', likesCount: '63'},
+    {id: 4, message: 'Post 4', likesCount: '45'},
+    {id: 5, message: 'Post 5', likesCount: '95'},
+]
+
 
 
 ReactDOM.render(
-    <App />,
+    <App posts={postsData} />,
   document.getElementById('root')
 );
