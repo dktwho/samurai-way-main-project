1bf32f2fad645ebcf23938af6497038c7a63d740
Alexander Korobov <dktwho@mail.ru>
2023-09-02 14:07:55 +0300

    add for reducers initialState and typed

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -1,6 +1,23 @@
-import {ActionsTypes} from "./store";
+import {ActionsTypes, MessagePageType} from "./store";
 
-export const dialogsReducer = (state: any, action: ActionsTypes) => {
+let initialState = {
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Hello'},
+        {id: 3, message: 'How are you?'},
+        {id: 4, message: 'Fine'},
+        {id: 5, message: 'Good'},
+    ],
+    dialogs: [
+        {id: 1, name: 'Bob'},
+        {id: 2, name: 'Bill'},
+        {id: 3, name: 'Sam'},
+        {id: 4, name: 'Tedd'},
+        {id: 5, name: 'John'},
+    ],
+    newMessageBody: '',
+}
+export const dialogsReducer = (state: MessagePageType = initialState, action: ActionsTypes) => {
     switch (action.type) {
         case 'UPDATE-NEW-MESSAGE-BODY': {
             state.newMessageBody = action.body
--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,7 +1,17 @@
-import {ActionsTypes, MyPostsType} from "./store";
+import {ActionsTypes, MyPostsType, ProfilePageType} from "./store";
 
 
-export const profileReducer = (state: any, action: ActionsTypes) => {
+let initialState = {
+    posts: [
+        {id: 1, message: 'Post 1', likesCount: '13'},
+        {id: 2, message: 'Post 2', likesCount: '22'},
+        {id: 3, message: 'Post 3', likesCount: '63'},
+        {id: 4, message: 'Post 4', likesCount: '45'},
+        {id: 5, message: 'Post 5', likesCount: '95'},
+    ],
+    newPostText: '',
+}
+export const profileReducer = (state: ProfilePageType = initialState, action: ActionsTypes) => {
     switch (action.type) {
 
         case 'ADD-POST': {
