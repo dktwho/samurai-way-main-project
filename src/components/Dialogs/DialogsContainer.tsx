a5986a16f67979744dd55e1201209ac31b60d7dc
Alexander Korobov <dktwho@mail.ru>
2023-09-04 12:07:51 +0300

    DialogsContainer change, add react-redux, add mapStateToProps, mapDispatchToProps, method CONNECT

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -5,27 +5,42 @@ import {
 } from "../../redux/store";
 import {Dialogs} from "./Dialogs";
 import {StoreContext} from "../../storeContext";
+import {connect} from "react-redux";
 
-export const DialogsContainer = () => {
-    return (
-        <StoreContext.Consumer>
-            {
-                store => {
-                    debugger
-                    let onSendMessageClick = () => {
-                        store.dispatch(sendMessageAC())
-                    }
+// export const DialogsContainer = () => {
+//     return (
+//         <StoreContext.Consumer>
+//             {
+//                 store => {
+//                     debugger
+//                     let onSendMessageClick = () => {
+//                         store.dispatch(sendMessageAC())
+//                     }
+//
+//                     let onNewMessageChange = (body: string) => {
+//                         store.dispatch(updateNewMessageBodyAC(body))
+//                     }
+//                     return <Dialogs updateNewMessageBody={onNewMessageChange}
+//                                     sendMessage={onSendMessageClick}
+//                                     dialogsPage={store.getState().dialogsPage}/>
+//                 }
+//             }
+//         </StoreContext.Consumer>
+//     )
+// };
 
-                    let onNewMessageChange = (body: string) => {
-                        store.dispatch(updateNewMessageBodyAC(body))
-                    }
-                    return <Dialogs updateNewMessageBody={onNewMessageChange}
-                                    sendMessage={onSendMessageClick}
-                                    dialogsPage={store.getState().dialogsPage}/>
-                }
-            }
-        </StoreContext.Consumer>
-    )
+let mapStateToProps = (state: any) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
+
+let mapDispatchToProps = (dispatch: any) => {
+    return {
+        updateNewMessageBody: (body: string)=> {dispatch(updateNewMessageBodyAC(body))},
+        sendMessage: () => {dispatch(sendMessageAC())}
+    }
+}
 
 
-};
+export const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from "./storeContext";
+import {Provider} from "react-redux";
+// import {Provider} from "./storeContext";
 
 let rerenderEntireTree = (state: any) => {
     ReactDOM.render(
