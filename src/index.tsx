b850a7b1bb8cdb32929ab6a19eaf3640d124a3c0
Alexander Korobov <dktwho@mail.ru>
2023-09-04 12:48:26 +0300

    modified dialogsReducer to immutable

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-// import {Provider} from "./storeContext";
 
 let rerenderEntireTree = (state: any) => {
     ReactDOM.render(
--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -20,15 +20,13 @@ let initialState = {
 export const dialogsReducer = (state: MessagePageType = initialState, action: ActionsTypes) => {
     switch (action.type) {
         case 'UPDATE-NEW-MESSAGE-BODY': {
-            state.newMessageBody = action.body
-            return state
+            return {...state, newMessageBody: action.body}
         }
 
         case 'SEND-MESSAGE': {
             let body = state.newMessageBody
             state.newMessageBody = ''
-            state.messages.push({id: new Date().getTime(), message: body})
-            return state
+            return {...state, messages: [...state.messages, {id: new Date().getTime(), message: body}]}
         }
 
         default : {
