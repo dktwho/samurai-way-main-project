23f6c3e147ab0b1274c2f84be715ed8fb6cb501f
Alexander Korobov <dktwho@mail.ru>
2023-09-28 11:41:08 +0300

    add to headers NavLink Login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
 export const Header = () => {
   return (
@@ -8,6 +9,9 @@ export const Header = () => {
         src="https://seeklogo.com/images/S/snapsvg-logo-8E936C9AE9-seeklogo.com.png"
         alt="logo"
       />
+        <div className={styled.loginBlock}>
+            <NavLink to={'./login'}>Login</NavLink>
+        </div>
     </header>
   );
 };
--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -2,12 +2,14 @@ import {combineReducers, legacy_createStore as createStore} from "redux";
 import {profileReducer} from "./profileReducer";
 import {dialogsReducer} from "./dialogsReducer";
 import {usersReducer} from "./usersReducer";
+import {authReducer} from "./authReducer";
 
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
+    auth: authReducer
 })
 
 export type RootReducerType = ReturnType<typeof rootReducer>
