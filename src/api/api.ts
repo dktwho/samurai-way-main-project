dbe1f9a868e950f83da1f49f5a86ad5da0d98bf6
Alexander Korobov <dktwho@mail.ru>
2023-09-29 16:22:16 +0300

    add to getUsers, one more promise, return only data

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,5 +4,5 @@ export const settings = {
     withCredentials: true
 }
 export const getUsers = (currentPage: number, pageSize: number) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, settings)
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, settings).then(res => res.data)
 }
