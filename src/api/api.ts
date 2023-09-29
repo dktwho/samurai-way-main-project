4732f1188e6d911f602bfa323ca3ceff3603ae04
Alexander Korobov <dktwho@mail.ru>
2023-09-29 16:37:29 +0300

    moved request logic in api file, follow/unfollow

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-export const settings = {
-    withCredentials: true
-}
 export const getUsers = (currentPage: number, pageSize: number) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, settings).then(res => res.data)
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {withCredentials: true}).then(res => res.data)
 }
+
+export const unfollow = (user: any) => {
+    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+        withCredentials: true, headers: {
+            "API-KEY": "706d8068-13d3-44f9-aada-12ec1dbdb516"
+        }
+    })
+}
+
+
+export const follow = (user: any) => {
+    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+        withCredentials: true, headers: {
+            "API-KEY": "706d8068-13d3-44f9-aada-12ec1dbdb516"
+        }
+    })
+}
+
--- src/components/Users/Users2.tsx
+++ src/components/Users/Users2.tsx
@@ -5,6 +5,7 @@ import {InitialStateType} from "../../redux/usersReducer";
 import {NavLink} from "react-router-dom";
 import {v4} from 'uuid';
 import axios from "axios";
+import {follow, unfollow} from "../../api/api";
 
 type PropsType = {
     onPageChanged: (pageNumber: number) => void
@@ -22,6 +23,8 @@ export const Users2 = (props: PropsType) => {
     for (let i = 1; i <= pagesCount; i++) {
         pagesArrCount.push(i)
     }
+
+
     return (
         <div>
             <div>
@@ -45,25 +48,20 @@ export const Users2 = (props: PropsType) => {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {
-                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {withCredentials: true, headers: {
-                                            "API-KEY": "706d8068-13d3-44f9-aada-12ec1dbdb516"
-                                        } })
-                                        .then((res) => {
-                                            if (res.data.resultCode === 0) {
-                                                props.unfollow(u.id)
-                                            }
-                                        })
-                                }}>Unfollow</button>
+                                ? <button onClick={() =>
+                                    unfollow(u).then((res) => {
+                                        if (res.data.resultCode === 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    })
+                                }>Unfollow</button>
+
                                 : <button onClick={() => {
-                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {withCredentials: true, headers: {
-                                            "API-KEY": "706d8068-13d3-44f9-aada-12ec1dbdb516"
-                                        }})
-                                        .then((res) => {
-                                            if (res.data.resultCode === 0) {
-                                                props.follow(u.id)
-                                            }
-                                        })
+                                    follow(u).then((res) => {
+                                        if (res.data.resultCode === 0) {
+                                            props.follow(u.id)
+                                        }
+                                    })
                                     props.follow(u.id)
                                 }}>Follow</button>}
 
@@ -83,6 +81,7 @@ export const Users2 = (props: PropsType) => {
                 </div>
             })}
         </div>
-    );
+    )
+        ;
 
 };
