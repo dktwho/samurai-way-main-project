e7fd4be1b6f90416d2213b6472c1055922b61ed3
Alexander Korobov <dktwho@mail.ru>
2023-10-04 19:33:34 +0300

    create getAuthUserDataThunkCreator, typed HeaderContainer for connect2

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -3,18 +3,15 @@ import {Header} from "./Header";
 import {connect} from "react-redux";
 import {getAuthUserDataThunkCreator} from "../../redux/authReducer";
 
-
 type MapStateToPropsType = {}
 type MapDispatchToPropsType = {
     getAuthUserDataThunkCreator: () => void
 }
 
-
 class HeaderContainer extends Component<MapStateToPropsType & MapDispatchToPropsType, unknown> {
     componentDidMount() {
         this.props.getAuthUserDataThunkCreator()
     }
-
 
     render() {
         return (
@@ -27,6 +24,5 @@ const mapStateToProps = (state: any) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login
 })
-
 
 export default connect(mapStateToProps, {getAuthUserDataThunkCreator})(HeaderContainer);
