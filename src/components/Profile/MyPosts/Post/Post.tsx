d970759686612c2d8619ed554319b6d83844cc31
Alexander Korobov <dktwho@mail.ru>
2023-10-17 17:28:59 +0300

    change avatar icon and logo profile icon

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -7,7 +7,9 @@ export const Post = ({message, likesCount}: MessagePropTypes) => {
 
     return (
         <div className={styled.item}>
-            <img src="https://rambabu.ca/assets/img/avatars/avatar.png" alt="avatar"/>
+            <img
+                src='https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg'
+                alt="avatar"/>
             {message}
             <div>
                 <span>{likesCount}</span>
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -16,8 +16,14 @@ export const ProfileInfo = (props: PropsType) => {
         <div>
             <div className={styled.descriptionBlock}>
                 <div>
+                    <div>
+                        <img width={'200px'} height={'200px'}
+                             src="https://img.freepik.com/premium-photo/cartoon-image-man-with-blue-shirt-orange-circle-with-words-s-it_745528-2802.jpg"
+                             alt="my-avatar"/>
+                    </div>
                     <div>Full name: {props.profile.fullName}</div>
-                    <img src={props.profile.photos.small} alt='profile-logo'/>
+                    {props.profile.photos.small ?
+                        <img src={props.profile.photos.small} alt='profile-logo'/> : 'User Photo:'}
                     <div>Instagram: {props.profile.contacts.instagram}</div>
                     <div>Github: {props.profile.contacts.github}</div>
                 </div>
