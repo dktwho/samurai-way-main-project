e16a1d7714fdb7cfd86bf55828e2c23ef3a80d83
Alexander Korobov <dktwho@mail.ru>
2023-10-17 16:56:42 +0300

    add setState to ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -3,16 +3,23 @@ import React from 'react';
 type StatusType = {
     status: string
 }
+
 class ProfileStatus extends React.Component<StatusType> {
     state = {
         editMode: false
+    }
+
+    activateEditMode() {
+        this.setState({
+            editMode: true
+        })
     }
 
     render() {
         return (
             <div>
                 {!this.state.editMode && <div>
-                    <span>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
                 </div>}
                 {this.state.editMode && <div>
                     <input value={this.props.status}/>
