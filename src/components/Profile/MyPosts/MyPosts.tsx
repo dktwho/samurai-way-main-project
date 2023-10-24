90c27f17fdce91ff54961d178199dbb59d3b718c
Alexander Korobov <dktwho@mail.ru>
2023-10-24 18:14:24 +0300

    add touched and error add styles for input and span error

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,6 +4,7 @@ import {Post} from './Post/Post';
 import {MyPostsType} from "../../../redux/store";
 import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import {TextArea} from "../../common/FormsControls/FormsControls";
 
 export type PostsTypeProps3 = {
     postsData: MyPostsType[]
@@ -24,7 +25,6 @@ export const MyPosts = (props: PostsTypeProps3) => {
     }
 
 
-
     return (
         <div className={styled.postBlock}>
             <h3> My Posts</h3>
@@ -39,8 +39,12 @@ const AddNewPostForm = (props: any) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field validate={[required, maxLength10]} name={'newPostText'} component={'textarea'}
-                       placeholder={'Enter your message'}/>
+                <Field validate={[required, maxLength10]}
+                       name={'newPostText'}
+                       component={TextArea}
+                       placeholder={'Post message'}
+
+                />
             </div>
             <div>
                 <button>Add post</button>
---
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -0,0 +1,17 @@
+import styles from './FormsControls.module.css'
+
+export const TextArea = ({input, meta, ...props}: any) => {
+
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
+            <div>
+                <textarea {...input} {...props} />
+            </div>
+            <div>
+                {hasError ? <span className={styles.error}>{meta.error}</span> : ''}
+            </div>
+        </div>
+
+    )
+}
