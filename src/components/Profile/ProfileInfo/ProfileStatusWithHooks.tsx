43b25167fa56e5bbe23c4c3b56fdcc9fa7abe25c
Alexander Korobov <dktwho@mail.ru>
2023-10-30 22:01:49 +0300

    modified component ProfileStatusWithHooks add two useState hook for editMode and status

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,21 +1,36 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type StatusType = {
     status: string
     updateStatus: (status: string) => void
 }
 const ProfileStatusWithHooks = (props: StatusType) => {
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    const activateMode = () => {
+        setEditMode(true)
+    }
+
+    const deActivateMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
+
     return (
         <div>
-            {<div>
+            {!editMode && <div>
                     <span
-                        onDoubleClick={() => {
-                        }}>{props.status || 'no status, click for insert'}</span>
+                        onDoubleClick={activateMode}>{props.status || 'no status, click for insert'}</span>
             </div>}
-            {false && <div>
-                <input onChange={() => {
-                }}
+            {editMode && <div>
+                <input onChange={onStatusChange}
                        autoFocus={true}
+                       onBlur={deActivateMode}
+                       value={status}
                 />
             </div>}
 
