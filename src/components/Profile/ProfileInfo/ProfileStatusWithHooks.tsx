c3205b68fafa44b203783e7428544905ed998e75
Alexander Korobov <dktwho@mail.ru>
2023-10-31 11:55:34 +0300

    add useEffect wit setStatus for ProfileStatusWithHooks

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type StatusType = {
     status: string
@@ -7,6 +7,10 @@ type StatusType = {
 const ProfileStatusWithHooks = (props: StatusType) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateMode = () => {
         setEditMode(true)
