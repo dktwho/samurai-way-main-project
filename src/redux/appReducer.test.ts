93004b2b8aa7b177fc78ef41ee4983ab7d052795
Alexander Korobov <dktwho@mail.ru>
2023-11-07 15:25:55 +0300

    add test appReducer

---
+++ src/redux/appReducer.test.ts
@@ -0,0 +1,12 @@
+import { setInitializedSuccessAC, appReducer } from './appReducer';
+
+describe('appReducer', () => {
+    it('should handle SET-INITIALIZED-SUCCESS-APP action', () => {
+        const initialState = { initialized: false };
+        const action = setInitializedSuccessAC();
+
+        const newState = appReducer(initialState, action);
+
+        expect(newState.initialized).toBe(true);
+    });
+});
