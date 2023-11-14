337715391732299b7f5f028456abfaed63c388bc
Alexander Korobov <dktwho@mail.ru>
2023-11-14 10:37:21 +0300

    test: modified appReducer.test

--- src/redux/appReducer.test.ts
+++ src/redux/appReducer.test.ts
@@ -1,12 +1,30 @@
-import { setInitializedSuccessAC, appReducer } from './appReducer';
+import {setInitializedSuccessAC, appReducer, setGlobalErrorAC} from './appReducer';
 
-describe('appReducer', () => {
-    // it('should handle SET-INITIALIZED-SUCCESS-APP action', () => {
-    //     const initialState = { initialized: false };
-    //     const action = setInitializedSuccessAC();
-    //
-    //     const newState = appReducer(initialState, action);
-    //
-    //     expect(newState.initialized).toBe(true);
-    // });
-});
+    describe('appReducer', () => {
+        it('should handle SET_INITIALIZED_SUCCESS action type', () => {
+            const initialState = {
+                initialized: false,
+                globalError: null,
+            };
+
+            const action = setInitializedSuccessAC();
+            const nextState = appReducer(initialState, action);
+
+            expect(nextState.initialized).toBe(true);
+        });
+
+        it('should handle SET_GLOBAL_ERROR action type', () => {
+            const initialState = {
+                initialized: false,
+                globalError: null,
+            };
+
+            const error = 'Something went wrong.';
+            const action = setGlobalErrorAC(error);
+            const nextState = appReducer(initialState, action);
+
+            expect(nextState.globalError).toBe(error);
+        });
+
+        // Add more test cases for other action types and scenarios as needed
+    });
