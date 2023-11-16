a2180d17b574b7270ee60739e38f99822de477ee
Alexander Korobov <dktwho@mail.ru>
2023-11-16 12:16:56 +0300

    feat:add redux type for appReducer

--- src/redux/appReducer.ts
+++ src/redux/appReducer.ts
@@ -1,5 +1,10 @@
 import {getAuthUserDataThunkCreator} from "./authReducer";
 
+// redux/ducks type
+const SET_INITIALIZED_SUCCESS_APP = 'app/SET-INITIALIZED-SUCCESS-APP'
+const SET_GLOBAL_ERROR = 'app/SET-GLOBAL-ERROR'
+
+
 // type state
 type AppDataType = {
     initialized: boolean,
@@ -16,21 +21,21 @@ type AppAllActions = SetInitializedACType | SetGlobalErrorACType
 
 export type SetInitializedACType = ReturnType<typeof setInitializedSuccessAC>
 export const setInitializedSuccessAC = () => {
-    return {type: 'SET-INITIALIZED-SUCCESS-APP'}  as const
+    return {type: SET_INITIALIZED_SUCCESS_APP}  as const
 }
 
 export type SetGlobalErrorACType = ReturnType<typeof setGlobalErrorAC>
 export const setGlobalErrorAC = (globalError: string | null) => {
-    return {type: 'SET-GLOBAL-ERROR', globalError } as const
+    return {type: SET_GLOBAL_ERROR, globalError } as const
 }
 
 export const appReducer = (state: AppDataType = initialState, action: AppAllActions):AppDataType  => {
     switch (action.type) {
-        case 'SET-INITIALIZED-SUCCESS-APP': {
+        case SET_INITIALIZED_SUCCESS_APP: {
             return {...state, initialized: true}
         }
 
-        case 'SET-GLOBAL-ERROR': {
+        case SET_GLOBAL_ERROR: {
             return {...state, globalError: action.globalError}
         }
         default : {
